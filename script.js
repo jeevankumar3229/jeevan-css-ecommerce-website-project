0ddbc1cc93f8124c4f839f10f494acf988a7eeca
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-10-16 21:52:56 +0530

    add script file

---
+++ script.js
@@ -0,0 +1,7 @@
+const ham=document.querySelector('.ham');
+const mobileMenu=document.querySelector('.mobile-menu');
+const body=document.querySelector('body')
+ham.addEventListener("click",function(){
+    mobileMenu.style.display="block";
+})
+
